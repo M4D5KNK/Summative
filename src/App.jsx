9180799c50ce52af76c36fd93b547aa56e96c333
Robledo <Tanesi.Robledo@student.tdsb.on.ca>
2025-06-12 13:02:14 -0400

    Firebase

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { StoreProvider } from './context';
+// import LoginView from './views/LoginView';
+// import AuthenticatedView from './views/AuthenticatedView';
+// import HomeView from './views/HomeView';
+// import RegisterView from './views/RegisterView';
+// import ProtectedRoutes from './utils/ProtectedRoutes';
+
+function App() {
+  return (
+    <StoreProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/register" element={<RegisterView />} />
+          <Route path="/login" element={<LoginView />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/authenticated" element={<AuthenticatedView />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
+  )
+}
+
+export default App
---
+++ src/firebase/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeGCMJcNBcI4EBfKleK45DjTESoAQ60ls",
+  authDomain: "cs4u-3076d.firebaseapp.com",
+  projectId: "cs4u-3076d",
+  storageBucket: "cs4u-3076d.firebasestorage.app",
+  messagingSenderId: "683132252148",
+  appId: "1:683132252148:web:ddefa89f2650d5e772d3a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseConfig);
+const firestore = getFirestore(firebaseConfig);
+
+export { auth, firestore };
